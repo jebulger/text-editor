4ad2dc893e0001103dec82630e4ce0095c19dea5
Jack <jebulger1003@gmail.com>
2023-08-03 13:39:51 -0400

    bug fixes to src-sw.js, database.js, and editor.js, committing package-locks

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,7 +16,7 @@ export const putDb = async (content) => {
   const db = await initdb();
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ value: content });
   const result = await request;
   return result;
 };
@@ -27,7 +27,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  return result;
+  return result.length > 0 ? result[0].value : null;
 };
 
 initdb();
